8eeab48cfc7131b537f7a7a6dd5f06bfc68db57d
Tsukasa140227 <68412494+Tsukasa140227@users.noreply.github.com>
2020-07-17 16:10:07 +0900

    Hiyokoattack

---
+++ Assets/Scripts/HiyokoGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiyokoGenerator : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject obj;
+    [SerializeField]
+    public float interval = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObj", 0.1f, interval);
+    }
+
+    // Update is called once per frame
+    void SpawnObj()
+    {
+        Instantiate(obj, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 8f;
+    [SerializeField]
+    public float movableRange = 5.5f;
+    [SerializeField]
+    public float power = 1000f;
+    [SerializeField]
+    public GameObject cannonBall;
+    [SerializeField]
+    public Transform spawnPoint;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0);
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, -movableRange, movableRange),
+            transform.position.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject newBullet = Instantiate(
+            cannonBall, spawnPoint.position, Quaternion.identity) as GameObject;
+        newBullet.GetComponent<Rigidbody2D>().AddForce(Vector3.up * power);
+    }
+}
